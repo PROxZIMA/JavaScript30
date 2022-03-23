1b97a32f0f9cd4387a8185c7ad2cb1e71be3864b
PROxZIMA <pratikbpingale9075@gmail.com>
2022-03-23 10:04:00 +0530

    Add: Day 22 - Follow Along Link Highlighter

---
+++ 22 - Follow Along Link Highlighter/scripts.js
@@ -0,0 +1,15 @@
+const pill = document.createElement('div');
+pill.classList.add('pill');
+document.body.appendChild(pill);
+
+pill.addEventListener('move', (e) => console.log(e));
+
+[...document.querySelectorAll('a')].forEach((href) => {
+  href.addEventListener('pointerenter', (e) => {
+    const coords = e.currentTarget.getBoundingClientRect();
+    console.log(coords, window.scrollY, window.scrollX);
+    pill.style.width = coords.width + 'px';
+    pill.style.height = coords.height + 'px';
+    pill.style.transform = `translate(${coords.left + window.scrollX}px, ${coords.top + window.scrollY}px)`;
+  })
+})
