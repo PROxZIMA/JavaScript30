24b5a329423320b815184ab5cc337256986fd5d1
PROxZIMA <pratikbpingale9075@gmail.com>
2022-04-05 08:35:00 +0530

    Update: Usage of logToHtml.js

--- 07 - Array Cardio Day 2/script.js
+++ 07 - Array Cardio Day 2/script.js
@@ -14,19 +14,21 @@ const comments = [
   { text: 'Ramen is my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 }
 ];
+console.log('People -> ');
 console.table(people);
+console.log('Comments -> ');
 console.table(comments);
 
 // Some and Every Checks
 // Array.prototype.every()
 const currYear = (new Date()).getFullYear();
-console.log("is at least one person 19 or older?", people.some(person => currYear - person['year'] > 19));
+console.log("is at least one person 19 or older? -> ", people.some(person => currYear - person['year'] > 19));
 // Array.prototype.some()
-console.log("is everyone 19 or older?", people.every(person => currYear - person['year'] > 19));
+console.log("is everyone 19 or older? -> ", people.every(person => currYear - person['year'] > 19));
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
-console.log("find the comment with the ID of 823423", comments.find(comment => comment['id'] === 823423));
+console.log("find the comment with the ID of 823423 -> ", comments.find(comment => comment['id'] === 823423));
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
