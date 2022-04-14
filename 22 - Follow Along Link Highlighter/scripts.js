6bfff187d79b431184baeee0edf0a7fe65b5dd35
PROxZIMA <pratikbpingale9075@gmail.com>
2022-04-14 11:12:00 +0530

    Refactor: Commeted coordinates object logging

--- 22 - Follow Along Link Highlighter/scripts.js
+++ 22 - Follow Along Link Highlighter/scripts.js
@@ -7,7 +7,7 @@ pill.addEventListener('move', (e) => console.log(e));
 [...document.querySelectorAll('a')].forEach((href) => {
   href.addEventListener('pointerenter', (e) => {
     const coords = e.currentTarget.getBoundingClientRect();
-    console.log(coords, window.scrollY, window.scrollX);
+    // console.log(coords, window.scrollY, window.scrollX);
     pill.style.width = coords.width + 'px';
     pill.style.height = coords.height + 'px';
     pill.style.transform = `translate(${coords.left + window.scrollX}px, ${coords.top + window.scrollY}px)`;
