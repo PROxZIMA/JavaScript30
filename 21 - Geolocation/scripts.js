80685c40d610781551a1381fc1325cc017b9a346
PROxZIMA <pratikbpingale9075@gmail.com>
2022-04-14 11:10:00 +0530

    Refactor: Commeted position object logging

--- 21 - Geolocation/scripts.js
+++ 21 - Geolocation/scripts.js
@@ -2,7 +2,7 @@ const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition((position) => {
-  console.log(position);
+  // console.log(position);
   speed.textContent = position.coords.speed || 0;
   arrow.style.transform = `rotate(${position.coords.heading || 0}deg)`;
 }, (e) => console.log(e));
